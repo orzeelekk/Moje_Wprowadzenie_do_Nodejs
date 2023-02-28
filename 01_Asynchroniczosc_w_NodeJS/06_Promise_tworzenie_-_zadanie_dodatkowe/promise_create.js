47b7bf39f9e1f0978f01d41c7259d225ae8ad9f6
orzeelekk <orzellpawell@gmail.com>
2023-02-28 16:36:23 +0100

    wyk zad 05

--- 01_Asynchroniczosc_w_NodeJS/06_Promise_tworzenie_-_zadanie_dodatkowe/promise_create.js
+++ 01_Asynchroniczosc_w_NodeJS/06_Promise_tworzenie_-_zadanie_dodatkowe/promise_create.js
@@ -1,3 +1,22 @@
 function myFavoriteCartoon(age) {
-
+    return new Promise((resolve,reject) => {
+        if (age >= 18) {
+            reject(
+                new Error("Proponowane filmy sa dla dzieci")
+            );
+        }
+        if (age === undefined) {
+            reject(
+                new Error("Podaj wiek")
+            );
+        }
+        if (age < 18) {
+            resolve (
+                ['Kot w butach', 'Shrek']
+            );
+        }
+    })
 }
+myFavoriteCartoon(92)
+myFavoriteCartoon(12)
+myFavoriteCartoon()
