ce833234a410015e88af36104341d7d7b6dfa1b2
orzeelekk <orzellpawell@gmail.com>
2023-02-28 13:01:08 +0100

    wyk zad 03

--- 01_Asynchroniczosc_w_NodeJS/03_Promise_API_cancel/promise_cancel.js
+++ 01_Asynchroniczosc_w_NodeJS/03_Promise_API_cancel/promise_cancel.js
@@ -4,7 +4,7 @@ const getRandomDelay = (maxDelay) => {
 
 const getAsyncNumbers = () => {
   var successDelay = getRandomDelay(10);
-
+  console.log(successDelay)
   return new Promise((resolve) => {
     setTimeout(
       () => resolve([1, 3, 5, 6, 4, 2]), 
@@ -12,3 +12,19 @@ const getAsyncNumbers = () => {
     );
   });
 };
+
+const racer1 = getAsyncNumbers();
+const racer2 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    reject(new Error("KONIEC CZASU, wygrał error"))
+  },3000)
+})
+
+Promise.race([racer1,racer2])
+    .then((wynik) => {
+      console.log(wynik + " KONIEC CZASU, wygrała tablica")
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+
