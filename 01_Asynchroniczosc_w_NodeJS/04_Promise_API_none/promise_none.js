c66e3db9be13d407375438013d23929b019dbe6d
orzeelekk <orzellpawell@gmail.com>
2023-02-28 13:26:18 +0100

    wyk zad 04

--- 01_Asynchroniczosc_w_NodeJS/04_Promise_API_none/promise_none.js
+++ 01_Asynchroniczosc_w_NodeJS/04_Promise_API_none/promise_none.js
@@ -1,16 +1,24 @@
-function promiseNone() {
+async function promiseNone(arg) {
+    const values = await Promise.allSettled(arg);
 
+    const isFulfilled = values.some((x) => {
+        console.log(x);
+        return x.status === 'fulfilled';
+    });
+
+    return new Promise((resolve, reject) => {
+        if (isFulfilled) {
+            reject("Heheh no i coś tam jednak jest resolved");
+        }
+
+        resolve("Wszystko źle więc dobrze");
+    });
 }
 
-promiseNone([
-  Promise.reject(),
-  Promise.reject(),
-  // Promise.resolve
-])
-  .then(() => {
-    console.log('All failed :)')
-  })
-  .catch(() => {
-    console.log('Something succeeded :(');
-  });
-  
+promiseNone([Promise.reject(), Promise.reject(), Promise.resolve()])
+    .then((res) => {
+        console.log("All failed :)", res);
+    })
+    .catch((err) => {
+        console.log("Something succeeded :(", err);
+    });
