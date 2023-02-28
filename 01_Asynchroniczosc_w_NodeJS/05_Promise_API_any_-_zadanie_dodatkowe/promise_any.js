5be132870955eb77e5fd0443cca3fca21b980e27
orzeelekk <orzellpawell@gmail.com>
2023-02-28 16:28:02 +0100

    wyk zad 05

--- 01_Asynchroniczosc_w_NodeJS/05_Promise_API_any_-_zadanie_dodatkowe/promise_any.js
+++ 01_Asynchroniczosc_w_NodeJS/05_Promise_API_any_-_zadanie_dodatkowe/promise_any.js
@@ -1,16 +1,29 @@
-function promiseAny() {
+async function promiseAny(arg) {
+  const values = await Promise.allSettled(arg);
+  const isFulfilled = values.some((x) => {
+    console.log(x);
+    console.log(x.status)
+    return x.status === 'fulfilled';
+  });
+
+  return new Promise((resolve, reject) => {
+    if (isFulfilled) {
+      resolve();
+    }
+    reject();
+  });
 
 }
 
 promiseAny([
   Promise.reject(),
   Promise.reject(),
-  // Promise.resolve
+  Promise.resolve
 ])
   .then(() => {
-    console.log('Something succeeded :)')
+    console.log('Something succeeded :) ')
   })
   .catch(() => {
-    console.log('All failed :(');
+    console.log('All failed :( ') ;
   });
   
