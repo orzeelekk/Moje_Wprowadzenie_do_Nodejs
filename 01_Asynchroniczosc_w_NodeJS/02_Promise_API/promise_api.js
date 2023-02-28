4585aea48565a3db91970b881b44bac9b8da76ad
orzeelekk <orzellpawell@gmail.com>
2023-02-28 12:39:15 +0100

    wyk zad 02

--- 01_Asynchroniczosc_w_NodeJS/02_Promise_API/promise_api.js
+++ 01_Asynchroniczosc_w_NodeJS/02_Promise_API/promise_api.js
@@ -8,9 +8,10 @@ const getAsyncNumbers = () => {
 
   return new Promise((resolve, reject) => {
     setTimeout(
-      () => resolve([1, 3, 5, 6, 4, 2]), 
+      () => resolve([1, 3, 5, 6, 4, 2]),
       successDelay
     );
+
 
     setTimeout(
       () => reject(new Error('No numbers found :(')), 
@@ -18,3 +19,92 @@ const getAsyncNumbers = () => {
     );
   });
 };
+
+// Promise.all([
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+//   getAsyncNumbers()
+// ])
+//     .then((results) => {
+//       console.log(results[0])
+//       console.log(results[1])
+//       console.log(results[2])
+//     })
+//     .catch((err) => {
+//       console.log(`Mamy errora - ${err.message}`)
+//     })
+//
+// Promise.all([
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+// ])
+//     .then(([response1, response2, response3]) => {
+//       const jointArray = response1.concat(response2, response3);
+//       const sortedArray = jointArray.sort();
+//       console.log(sortedArray);
+//     })
+//     .catch((error) => {
+//       console.log(`Error occurred: ${error.message}`);
+//     });
+//
+// Promise.allSettled([
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+//   getAsyncNumbers()
+// ])
+//     .then((results) => {
+//       console.log(results[0].status + " suchy status")
+//       console.log(results[0])
+//       console.log(results[1])
+//       console.log(results[2])
+//     })
+//     .catch((err) => {
+//       console.log(`Mamy errora - ${err.message}`)
+//     })
+// Promise.allSettled([
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+//   getAsyncNumbers(),
+// ])
+//     .then((results) => {
+//       return results.map((result) => {
+//         return result.status;
+//       });
+//     })
+//     .then((statuses) => {
+//       console.log(statuses[0]);
+//       console.log(statuses[1]);
+//       console.log(statuses[2]);
+//     })
+//     .catch((error) => {
+//       console.log(`Error ocurred: ${error.message}`);
+//     });
+//
+Promise.all([
+  Promise.resolve(1),
+  Promise.resolve(3),
+  Promise.resolve(1500100900),
+])
+    .then((results) => {
+      console.log(results[0]);
+      console.log(results[1]);
+      console.log(results[2]);
+    })
+    .catch((error) => {
+      console.log(`Error ocurred: ${error.message}`); // never shows up
+    });
+
+Promise.all([
+  Promise.reject(1),
+  Promise.reject('error'),
+  Promise.reject(new Error('new error')),
+])
+    .then((results) => {
+      console.log(results[0]);
+      console.log(results[1]);
+      console.log(results[2]);
+    })
+    .catch((error) => {
+      console.log('Error ocurred:', error); // always shows up
+    });
