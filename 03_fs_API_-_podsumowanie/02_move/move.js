6342ca0de97a057c4e3a273da6e6b23d4f85d7e6
orzeelekk <orzellpawell@gmail.com>
2023-03-01 14:23:02 +0100

    wyk zad 02

--- 03_fs_API_-_podsumowanie/02_move/move.js
+++ 03_fs_API_-_podsumowanie/02_move/move.js
@@ -1,12 +1,28 @@
 import fs from "fs";
+import path from 'path';
 
-const move = async(path1,path2) => {
-    const fileValue = await js.promises.readFile(path1);
+const createStructure = async () => {
+    try {
+        await fs.promises.mkdir('./dir_one', {recursive: true});
+        await fs.promises.mkdir('./dir_two', {recursive: true});
+        await fs.promises.writeFile('./dir_one/file1.txt','ugabuga');
+    } catch (err) {
+        console.log(err)
+    }
+}
+async function moveFile(file,fileLocation) {
+    try {
+        await createStructure()
+        const basename = path.basename(file);
+        const newFile = path.extname(fileLocation) ? fileLocation : path.join(fileLocation, basename);
+        const fileData = await fs.promises.readFile(file, "utf-8");
 
-    await fs.promises.writeFile(path2,fileValue);
-    await fs.promises.unlink(path1);
+        await fs.promises.writeFile(newFile, fileData);
+
+        await fs.promises.unlink(file);
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-(async() => {
-    await move (`./one.txt`,`./fir/two.txt`)
-})();
+moveFile('./dir_one/file1.txt', './dir_two/test.txt');
