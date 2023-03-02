73a72cbcb66e2f0984d9b4f751a5fd698c21506b
orzeelekk <orzellpawell@gmail.com>
2023-03-02 14:50:21 +0100

    wyk zad 05

--- 04_Node_Event_Emitter/05_Rozszerzanie_klasy_EventEmitter/extends.js
+++ 04_Node_Event_Emitter/05_Rozszerzanie_klasy_EventEmitter/extends.js
@@ -1,19 +1,37 @@
-class Task {
+import { EventEmitter } from 'events';
+
+class Task extends EventEmitter {
   start() {
     console.log('Started');
+    this.emit('started')
   }
   stop() {
     console.log('Stopped');
+    this.emit('stopped')
   }
   pause() {
     console.log('Paused');
+    this.emit('paused')
   }
   resume() {
     console.log('Resumed');
+    this.emit('resumed')
   }
 }
 
 const task = new Task();
+task.once('started',() => {
+  console.log('Zaczynamy...')
+})
+task.on('paused',() => {
+  console.log('-Wstrzymane-')
+})
+task.on('resumed',() => {
+  console.log('-DALEJ-')
+})
+task.once('stopped',() => {
+  console.log('...Koniec')
+})
 task.start();
 task.pause();
 task.resume();
