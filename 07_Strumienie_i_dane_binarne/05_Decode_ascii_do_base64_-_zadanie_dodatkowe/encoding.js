a5f588321b247344be2f6de615e0def90cccf313
orzeelekk <orzellpawell@gmail.com>
2023-03-06 14:51:49 +0100

    wtk zad 05

--- 07_Strumienie_i_dane_binarne/05_Decode_ascii_do_base64_-_zadanie_dodatkowe/encoding.js
+++ 07_Strumienie_i_dane_binarne/05_Decode_ascii_do_base64_-_zadanie_dodatkowe/encoding.js
@@ -2,5 +2,11 @@ const fs = require('fs')
 
 const message = 'Ik5pZSBkb3R5a2HEhyEiIC0gbXVzaSBiecSHIGplZG7EhSB6ZSBzdHJhc3puaWVqc3p5Y2ggcnplY3p5IGRvIHByemVjenl0YW5pYSB3IGrEmXp5a3UgQnJhaWxsYS4uLg=='
 
-const result = Buffer.from(message,'base64').toString('utf-8')
-console.log(result)
+const utf = Buffer.from(message,'base64').toString('utf-8')
+const ascii = Buffer.from(message,'base64').toString('ascii')
+
+const writeUtf = fs.createWriteStream(__dirname + '/targer.utf.txt')
+const writeAscii = fs.createWriteStream(__dirname + '/targer.ascii.txt')
+
+writeUtf.write(utf);
+writeAscii.write(ascii);
