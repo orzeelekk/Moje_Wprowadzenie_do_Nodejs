16786f4d408cfd936dcfd7df42fc40c1b3551786
orzeelekk <orzellpawell@gmail.com>
2023-03-06 14:36:11 +0100

    wyk zad 02

--- 07_Strumienie_i_dane_binarne/02_pipe/pipe.js
+++ 07_Strumienie_i_dane_binarne/02_pipe/pipe.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const input = __dirname + '/input.txt'
+// czemu te dwie podlogi??
 const readStream = fs.createReadStream(input)
 
 const output = __dirname + '/output.txt'
