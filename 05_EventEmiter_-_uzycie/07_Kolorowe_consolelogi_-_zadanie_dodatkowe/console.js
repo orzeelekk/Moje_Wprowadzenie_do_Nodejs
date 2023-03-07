4e982065ef5051a89915438e77e420cfd829ab37
orzeelekk <orzellpawell@gmail.com>
2023-03-07 18:02:10 +0100

    wyk zad 07

--- 05_EventEmiter_-_uzycie/07_Kolorowe_consolelogi_-_zadanie_dodatkowe/console.js
+++ 05_EventEmiter_-_uzycie/07_Kolorowe_consolelogi_-_zadanie_dodatkowe/console.js
@@ -34,3 +34,82 @@ const backgrounds = {
   BgWhite: '\x1b[47m',
 };
 
+const emitter = new EventEmitter();
+const result = {
+  effect: '\x1b[0m',
+  fgColor: '\x1b[37m',
+  bgColor: '\x1b[40m',
+  text: `SampleText`,
+};
+
+function myCrazyConsoleLog({ effect, fgColor, bgColor, text }) {
+  console.log(effect, fgColor, bgColor, text, '\x1b[0m');
+}
+
+emitter.on('effect', (selected) => {
+  result.effect = effects[selected];
+});
+
+emitter.on('fgColor', (selected) => {
+  result.fgColor = foregrounds[selected];
+});
+
+emitter.on('bgColor', (selected) => {
+  result.bgColor = backgrounds[selected];
+});
+
+emitter.on('text', (selected) => {
+  result.text = selected;
+});
+
+emitter.on('complete', (result) => {
+  myCrazyConsoleLog(result);
+});
+
+async function flow() {
+  try {
+    const { effect } = await prompt([
+      {
+        type: 'list',
+        name: 'effect',
+        message: 'Choose an effect',
+        choices: Object.keys(effects),
+      },
+    ]);
+    console.log(effect);
+
+    emitter.emit('effect', effect);
+    const { foreground } = await prompt([
+      {
+        type: 'list',
+        name: 'foreground',
+        message: 'Choose foreground color',
+        choices: Object.keys(foregrounds),
+      },
+    ]);
+    emitter.emit('fgColor', foreground);
+
+    const { background } = await prompt([
+      {
+        type: 'list',
+        name: 'background',
+        message: 'Choose background color',
+        choices: Object.keys(backgrounds),
+      },
+    ]);
+    emitter.emit('bgColor', background);
+
+    const { sentence } = await prompt([
+      {
+        name: 'sentence',
+        message: 'Give some sample text:'
+      },
+    ]);
+    emitter.emit('text', sentence);
+    emitter.emit('complete', result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+flow();
