59796e8da644fa1ef02739c7c752930bedc5ae5f
orzeelekk <orzellpawell@gmail.com>
2023-03-07 16:40:30 +0100

    wyk zad 04

--- 05_EventEmiter_-_uzycie/04_fs_API_revisited/read_write.js
+++ 05_EventEmiter_-_uzycie/04_fs_API_revisited/read_write.js
@@ -3,20 +3,45 @@ import fs from 'fs';
 import Path from 'path';
 
 const emitter = new EventEmitter();
-
-emitter.on('write',(path,text) => {
-    fs.writeFile(path,text, () => {
-        emitter.emit('read',path);
-    })
+// to jest nasluch sam plik write na dole
+emitter.on('write', () => {
+    fs.readFile('./myTxt.txt', 'utf-8', (err, data) => {
+        if (err) {
+            emitter.emit('error', new Error(err));
+            return;
+        }
+        emitter.emit('read', data);
+    });
 });
 
-emitter.on('read',(path) => {
-    console.log(fs.readFileSync(path))
-    //readFileSync??
+emitter.on('read', (data) => {
+    console.log(data);
 });
 
-fs.readFile('./file.txt',(err,text) => {
-    console.log(text)
-    emitter.emit('write','file2.txt',text)
+emitter.on('error', (err) => {
+    console.log(err);
 });
+
+fs.writeFile('./myTxt.txt', 'Some text here', (err) => {
+    if (err) {
+        emitter.emit('error', new Error(err));
+        return;
+    }
+    emitter.emit('write');
+});
+// emitter.on('write',(path,text) => {
+//     fs.writeFile(path,text, () => {
+//         emitter.emit('read',path);
+//     })
+// });
+//
+// emitter.on('read',(path) => {
+//     console.log(fs.readFileSync(path,'utf-8'))
+//     //readFileSync na to szyfrowane
+// });
+//
+// fs.readFile('./file.txt','utf-8',(err,text) => {
+//     console.log(text)
+//     emitter.emit('write','file2.txt',text)
+// });
 
