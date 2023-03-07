cf36c8725fb7604175083659fc8919002b932b93
orzeelekk <orzellpawell@gmail.com>
2023-03-07 17:40:55 +0100

    wyk zad 05

--- 05_EventEmiter_-_uzycie/05_fizbaz/fizbaz.js
+++ 05_EventEmiter_-_uzycie/05_fizbaz/fizbaz.js
@@ -2,5 +2,26 @@ import { EventEmitter } from 'events';
  const emitter = new EventEmitter();
 
  emitter.on('number',(number) => {
-
+  if (number % 3 === 0 && number % 5 ===0) {
+   console.log('fizbaz')
+  }
  })
+ emitter.on('number',(number) => {
+  if (number % 3 === 0 && number % 5 !==0) {
+   console.log('fiz')
+  }
+ })
+ emitter.on('number',(number) => {
+  if (number % 3 !== 0 && number % 5 ===0) {
+   console.log('baz')
+  }
+ })
+ emitter.on('number',(number) => {
+  if (number % 3 !== 0 && number % 5 !==0) {
+   console.log(number)
+  }
+ })
+
+for (let i = 0; i <= 100; i++) {
+ emitter.emit('number',i);
+}
