ae563d729d7e816a29a66b698c12c0239df49bda
orzeelekk <orzellpawell@gmail.com>
2023-03-08 16:03:46 +0100

    wyk zad 03

--- 08_Bufory_i_kodowanie/03_Gold_mining/goldMiner.js
+++ 08_Bufory_i_kodowanie/03_Gold_mining/goldMiner.js
@@ -1,1 +1,34 @@
 const fs = require('fs')
+
+const readStream = fs.createReadStream(__dirname + '/input')
+
+let results = []
+
+readStream.on('data',(data) => {
+    const utf = data.toString()
+    console.log(utf)
+    const buffer64 = Buffer.from(utf,'base64')
+    // console.log(buffer64);
+    const resultUtf = buffer64.toString('utf-8');
+    // console.log(resultUtf)
+    results.push(...resultUtf.match(/{([^}]*)}/g));
+})
+readStream.on('close', () => {
+    console.log(results)
+    console.log(itemsFilter(results))
+})
+
+const itemsFilter = results => {
+    const uniqKeys = Array.from(new Set(results))
+    console.log(uniqKeys)
+    return uniqKeys.reduce((accumulator, currentValue) => {
+        const occurances = () => results.filter(item => item === currentValue).length
+        return {
+            //nasze ...zbiory (bez tego robi tylko po 1 szukanej)
+            ...accumulator,
+            [`${currentValue}`]: occurances()
+        }
+    }, {})
+
+}
+
