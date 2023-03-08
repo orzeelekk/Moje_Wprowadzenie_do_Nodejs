66253f17bf1cfe2859e1bb348d7265d4ace22ce5
orzeelekk <orzellpawell@gmail.com>
2023-03-08 13:15:51 +0100

    wyk zad 03

--- 06_Czym_sa_strumienie/03_Przekazanie_strumienia/inputToOutput.js
+++ 06_Czym_sa_strumienie/03_Przekazanie_strumienia/inputToOutput.js
@@ -1,1 +1,9 @@
 const fs = require('fs')
+
+const readStream = fs.createReadStream('../02_Strumien_zapisu/target.txt')
+const writeStream = fs.createWriteStream('./output.txt')
+writeStream.on('pipe',(sourceStream) => {
+    console.log(`Jakies info ${sourceStream}`)
+})
+readStream
+    .pipe(writeStream)
