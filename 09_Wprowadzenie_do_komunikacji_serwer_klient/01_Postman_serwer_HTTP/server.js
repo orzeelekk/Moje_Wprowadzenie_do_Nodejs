0b84bd79be656d0dfb0161928022a2f8ea3ff71d
orzeelekk <orzellpawell@gmail.com>
2023-03-09 13:29:35 +0100

    wyk zad 01

--- 09_Wprowadzenie_do_komunikacji_serwer_klient/01_Postman_serwer_HTTP/server.js
+++ 09_Wprowadzenie_do_komunikacji_serwer_klient/01_Postman_serwer_HTTP/server.js
@@ -1,23 +1,35 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer(async (req, res) => {
     if (req.method === 'GET' && req.url === '/user') {
         res.end('Server received a GET request to path /user')
     }
-    if (req.method === 'GET') {
+    else if (req.method === 'GET') {
         res.end('Server received a GET request')
     }
-    if (req.method === 'POST') {
-        res.end('Server received a POST request')
+    else if (req.method === 'POST' && req.url === "/file") {
+        const writeStream = fs.createWriteStream(__dirname + '/file.txt')
+        // console.log(req)
+        req.pipe(writeStream)
+        res.end('Wysłano plik do zapisu')
+        // req.setEncoding('utf-8');
+        // let file = '';
+        // req.on('data',(data) => {
+        //     file += data;
+        // })
+        // res.end('Server received a POST request')
     }
-    if (req.method === 'PUT') {
+    else if (req.method === 'PUT') {
         res.end('Server received a PUT request')
     }
-    if (req.method === 'PATCH') {
+    else if (req.method === 'PATCH') {
         res.end('Server received a PATCH request')
+    } else {
+        res.statusCode = 404;
+        res.end()
     }
 
-    res.end()
 })
 
 server.listen(8080, () => {
